374930a3870f1cfac8d15e1b255534f33270ea2e
Jorge Alegria <alegre09@live.com>
2016-02-01 22:56:55 -0700

    añadi etiquetas al ejemplo de box

--- BoxLayoutDemo1.java
+++ BoxLayoutDemo1.java
@@ -14,16 +14,17 @@ import javax.swing.JPanel;
 			LayoutManager layout = new BoxLayout(panel, BoxLayout.X_AXIS);
 			panel.setLayout(layout);
 
-			panel.add(new JLabel("a"));
-			panel.add(new JLabel("b"));
-			panel.add(new JLabel("c"));
-			panel.add(new JLabel("d"));
-			panel.add(new JLabel("e"));
-			panel.add(new JLabel("f"));
-
+			panel.add(new JLabel("Ejemplo de BoxLayout de puro Texto "));
+			panel.add(new JLabel(" a,"));
+			panel.add(new JLabel(" b,"));
+			panel.add(new JLabel(" c,"));
+			panel.add(new JLabel(" d,"));
+			panel.add(new JLabel(" e,"));
+			panel.add(new JLabel(" f,"));
+			panel.add(new JLabel(" este es el BoxLayout más simple."));
 			frame.add(panel);
 			
-			frame.setSize(300,200);
+			frame.setSize(700,300);
 			frame.setVisible(true);
 		}
 
